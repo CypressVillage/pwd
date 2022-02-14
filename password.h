32d687f21a7d192e162ed29d0de52fb49b4101e7
CypressVillage <2169626027@qq.com>
2022-02-14 08:36:16 +0000

    update password.h. 添加文件操作

--- password.h
+++ password.h
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
-  char zhanghao[30];
-  char zhanghu[30];
-  char name[30];
-  char pwd[30];
-  char lnk[2];
-} Password;//用Password代替struct类型
+#ifndef __PASSWORD_H__
+#define __PASSWORD_H__
+  typedef struct{
+    char zhanghao[30];
+    char zhanghu[30];
+    char name[30];
+    char pwd[30];
+    char lnk[2];
+  } Password;//鐢≒assword浠ｆ浛struct绫诲瀷
+  
+  void show_operation( void );
+  void show( Password *p, int nums );
+  void list_all( Password *p, int nums );
+  void store_password( Password *p, int nums );
+  void change( Password *p, int nums );
+  void my_delete( Password *p, int nums );
+  
+  void search_by_lnk( char *input, Password *p, int nums );
+  void search_by_zhanghao( char *input, Password *p, int nums );
+  void search_by_zhanghu( char *input, Password *p, int nums );
+  void search_by_name( char *input, Password *p, int nums );
+  void search( Password *p, int nums );
 
-void show( Password *p, int nums );
-void list_all( Password *p, int nums );
-void store_password( Password *p, int nums );
-void change( Password *p, int nums );
-void my_delete( Password *p, int nums );
+  void password_panel( Password *p_pwd );
 
-void search_by_lnk( char *input, Password *p, int nums );
-void search_by_zhanghao( char *input, Password *p, int nums );
-void search_by_zhanghu( char *input, Password *p, int nums );
-void search_by_name( char *input, Password *p, int nums );
-void search( Password *p, int nums );
+  #endif
