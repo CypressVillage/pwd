2b21c9567a588d679b0271fe3ffcbc46d0dbfcb3
CypressVillage <2169626027@qq.com>
2022-02-17 12:36:24 +0000

    ver1.1

---
+++ fileoperation.h
@@ -0,0 +1,21 @@
+#ifndef FILEOPERATION_H
+#define FILEOPERATION_H
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include "password.h"
+
+    #define MAX_FILE_PATH_LEN 100
+
+
+    void dbg( char* tosay );
+    void welcome( void );
+    void show_file_operation( void );
+    void my_exit( void );
+
+    FILE *select_file( FILE *file );
+    Password *read_file( FILE *file );
+    void save_file( Password *ppwd );
+    FILE *file_panel( FILE *file );
+
+#endif
