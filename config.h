e177210bf465a895951e2b653f8fcd79b009245f
CypressVillage <2169626027@qq.com>
2022-03-02 12:03:51 +0000

    ver1.2.1

--- config.h
+++ config.h
@@ -1,17 +1,20 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-
+    // #include "cJSON.h"
 
     #define MAX_CONFIG_LENGTH 50
     #define MAX_FILE_PATH_LEN 100
 
 
-    /************************************/
+    /*config list*/
     extern char file_path[MAX_FILE_PATH_LEN];
     extern char file_name[100];
+    extern char *PasswordPlus;
+    extern int IsEnablePasswordPlus;
     extern int Language;
-    extern int SplitLineStyle;
-    /************************************/
+    extern char *SplitLineStyle;
+    extern int IsDeveloperMode;
+    extern int IsCls;
 
     typedef struct configList{
         char config[MAX_CONFIG_LENGTH];
@@ -19,12 +22,15 @@
         struct configList *next;
     } ConfigList;
 
+    void show_config_operation( void );
+    void show_all_config(void);
     ConfigList *read_config( void );
     void list_config( ConfigList *cp );
     void set_config( ConfigList *cp );
     ConfigList *change_config( ConfigList *cp );
+    ConfigList *refresh_config_list( void );
     void save_config( ConfigList *cp );
-    void config_panel( void );
+    void config_panel( ConfigList *cp );
 
 
 #endif
