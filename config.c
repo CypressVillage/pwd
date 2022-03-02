2477b2f0f330e75e8740c2dd018efcb64a690d37
CypressVillage <2169626027@qq.com>
2022-03-02 12:04:31 +0000

    ver1.2.1

--- config.c
+++ config.c
@@ -11,15 +11,56 @@
 */
 char file_path[MAX_FILE_PATH_LEN] = "D://zbc//biancheng//Environment//my_environment//pwd//";
 char file_name[100] = "pwd_data";
+char *PasswordPlus = "1";
+int IsEnablePasswordPlus = 0;
 int Language = 0;
-int SplitLineStyle = 0;
+char *SplitLineStyle = "---";
+int IsDeveloperMode = 0;
+int IsCls = 0;
 /*******************************/
+
+void show_config_operation( void ){
+    switch (Language){
+        case 0:
+            printf("%-20s","1: 查看设置");
+            printf("%-20s","2: 更改设置");
+            printf("%-20s","3: 恢复出厂");
+            printf("\n");
+            printf("%-20s","6: 更新设置");
+            printf("%-20s","7: 应用设置");
+            printf("%-20s","8: 保存设置");
+            printf("\n");
+            printf("%-20s","9: 查看操作");
+            printf("%-20s","0: 退出系统");
+            printf("%-20s","-: 返回上级");
+            printf("\n");
+
+            if(IsDeveloperMode){
+                printf("%-16s","l: list config");
+                printf("%-16s","p: show path");
+                printf("%-16s","c: change password plus");
+                printf("\n");
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+void show_all_config(void){
+    printf("语言:");
+    if(Language){printf("英文\n");} else{printf("中文\n");}
+    printf("分割线样式:");
+    if(!strcmp(SplitLineStyle, "---")) printf("---\n");
+    if(!strcmp(SplitLineStyle, "num")) printf("数字\n");
+}
+
 
 ConfigList *read_config( void ){
 
-    char *total_path = malloc( 200 * sizeof(char) );
-    strcpy( total_path, file_path );
-    total_path = strcat( total_path, "config.pwd" );
+    char *total_path = strcat( get_file_path(), "config.pwd" );
 
     FILE *fp = fopen( total_path, "r" );
     if( fp == NULL ){
@@ -49,6 +90,7 @@ ConfigList *read_config( void ){
     }
 }
 
+
 void list_config( ConfigList *cp ){
     while(cp!=NULL){
         printf("%s:", cp->config);
@@ -57,24 +99,110 @@ void list_config( ConfigList *cp ){
     }
 }
 
+
 void set_config( ConfigList *cp ){
     while(cp!=NULL){
-        /*******************************/
-        if( !strcmp((char*)(cp->config), "Language") ){
-            if( !strcmp((char*)(cp->mode),"Chinese") ) Language = 0;
-            if( !strcmp((char*)(cp->mode),"English") ) Language = 1;
+        /*****************************************************************************/
+        if( !strcmp(cp->config, "Language") ){
+            if( !strcmp(cp->mode,"Chinese") ) Language = 0;
+            if( !strcmp(cp->mode,"English") ) Language = 1;
         }
-        if( !strcmp((char*)(cp->config), "SplitLineStyle") ){
-            if( !strcmp((char*)(cp->mode),"---") ) SplitLineStyle = 0;
-            if( !strcmp((char*)(cp->mode),"Number") ) SplitLineStyle = -1;
+        if( !strcmp(cp->config, "SplitLineStyle") ){
+            if( !strcmp(cp->mode,"---") ) SplitLineStyle = "---";
+            if( !strcmp(cp->mode,"Number") ) SplitLineStyle = "num";
         }
-
-
-        /*******************************/
+        if( !strcmp(cp->config, "IsEnablePasswordPlus") ){
+            if( !strcmp(cp->mode,"false") ) IsEnablePasswordPlus = 0;
+            if( !strcmp(cp->mode,"true") ) IsEnablePasswordPlus = 1;
+        }
+        if( !strcmp(cp->config, "PasswordPlus") ){
+            PasswordPlus = malloc( 100 * sizeof(char) );
+            strcpy( PasswordPlus, cp->mode );
+        }
+        if( !strcmp(cp->config, "IsCls") ){
+            if( !strcmp(cp->mode,"false") ) IsCls = 0;
+            if( !strcmp(cp->mode,"true") ) IsCls = 1;
+        }
+        /*****************************************************************************/
         cp = cp->next;
     }
 }
 
+
+/*a panel to user, to change global vars*/
 ConfigList *change_config( ConfigList *cp ){}
-void save_config( ConfigList *cp ){}
-void config_panel( void ){}
+
+
+/*read global vars, create a new config list*/
+ConfigList *refresh_config_list( void ){
+    ConfigList *cp;
+    if(Language != 0){
+        cp->next = malloc(sizeof(ConfigList));
+        strcpy(cp->config,"Languange");
+        if(Language==1){
+            strcpy(cp->mode,"Chinese");
+        }
+    }
+    if(IsEnablePasswordPlus != 0){
+        cp->next = malloc(sizeof(ConfigList));
+        cp = cp->next;
+        strcpy(cp->config,"IsEnablePasswordPlus");
+        strcpy(cp->mode,"true");
+    }
+}
+
+
+/*read config list into file*/
+void save_config( ConfigList *cp ){
+    char *total_path = strcat( get_file_path(), "config.pwd" );
+    FILE *file = fopen( total_path, "w" );
+
+    int line_num = 1;
+    while( cp!=NULL ){
+
+        switch( line_num % 2 ){
+            case 1: {fprintf( file, "%s\n", cp->config ); break;}
+            case 0: {fprintf( file, "%s\n", cp->mode ); cp=cp->next; break;}
+        }
+
+        line_num ++;
+    }
+
+    fclose( file );
+    printf("保存成功\n");
+}
+
+
+void config_panel( ConfigList *cp ){
+    if(IsCls) system("cls");
+    printf("设置:\n");
+    split_line();
+    show_config_operation();
+
+    int mode = 0;
+    while(1){
+        fflush(stdin);
+        split_line();
+        printf("请选择操作:\n");
+
+        scanf("%c", &mode);
+        switch(mode){
+            case '1':{ show_all_config(); break;}
+            case '2':{  break;}
+            case '3':{  break;}
+            case '4':{  break;}
+            case '7':{ set_config(cp); break;}
+            case '8':{ save_config(cp); break;}
+            case '9':{ show_config_operation(); break;}
+            case '0':{ my_exit(); break;}
+            case '-':{ FILE *file = NULL; file_panel( file ); break;}
+            case '`':{ IsDeveloperMode = 1; break;}
+            case 'l':{ if(IsDeveloperMode) list_config(cp); break;}
+            case 'p':{ if(IsDeveloperMode) {printf(get_file_path()); printf("\n");}break;}
+            case 'c':{ if(IsDeveloperMode) change_password_plus(); break;}
+            default: break;
+        }
+
+    }
+
+}
