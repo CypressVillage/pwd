acfe9db325853cc84de14e3a3d1726c369a2f28c
CypressVillage <2169626027@qq.com>
2022-03-02 12:02:37 +0000

    ver1.2.1

--- password.h
+++ password.h
@@ -4,8 +4,8 @@
 #ifndef PASSWORD_H
 #define PASSWORD_H
   typedef struct password{
-    char zhanghao[30];
-    char zhanghu[30];
+    char account[30];
+    char account_num[30];
     char name[30];
     char pwd[30];
     char lnk[2];
@@ -14,17 +14,11 @@
   void split_line( void );
   void show_operation( void );
   void show( Password *p, int nums );
-  void list_all( Password *p, int nums );
-  void new_password( Password *p, int nums );
-  void change( Password *p, int nums );
-  void my_delete( Password *p, int nums );
-  
-  void search_by_lnk( char *input, Password *p, int nums );
-  void search_by_zhanghao( char *input, Password *p, int nums );
-  void search_by_zhanghu( char *input, Password *p, int nums );
-  void search_by_name( char *input, Password *p, int nums );
-  void search( Password *p, int nums );
-
+  void list_all( Password *p );
+  void new_password( Password *p );
+  void change( Password *p );
+  void my_delete( Password *p );
+  void search( Password *p );
   void password_panel( Password *p_pwd );
 
 #endif
