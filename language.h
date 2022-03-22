107d9e3c6dd1d0354191e1a98e38a3682bc774c9
CypressVillage <2169626027@qq.com>
2022-03-22 11:55:33 +0000

    add language.h.

---
+++ language.h
@@ -0,0 +1,11 @@
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+
+    /*
+    * password is 1, fileoperation is 2, config is 3
+    */
+    void language_1_new_password(char *pre, int num);
+    void language_1_search(char *pre, int num);
+
+    
+#endif
